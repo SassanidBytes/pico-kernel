f2cb8307730ba9e727c97561390b84f0d24f18cc
SassanidBytes <sassanidbytes@gmail.com>
2026-05-22 00:25:31 +0200

    Add boot splash screen with logo

--- src/main.c
+++ src/main.c
@@ -1,14 +1,34 @@
 #include "pico/stdlib.h"
 #include "drivers/st7789.h"
 #include "kernel/shell.h"
+#include "logo.h"
+
+void show_splash() {
+    // Hintergrund schwarz
+    st7789_fill(0x0000);
+
+    // Logo zentriert anzeigen
+    int x_offset = (DISPLAY_WIDTH - LOGO_W) / 2;
+    int y_offset = (DISPLAY_HEIGHT - LOGO_HEIGHT) / 2;
+    for (int y = 0; y < LOGO_HEIGHT; y++) {
+        for (int x = 0; x < LOGO_W; x++) {
+            uint16_t color = logo_data[y * LOGO_W + x];
+            st7789_set_pixel(x + x_offset, y + y_offset, color);
+        }
+    }
+
+    // 3 Sekunden anzeigen
+    sleep_ms(3000);
+}
 
 int main() {
-    // stdio via USB initialisieren
     stdio_init_all();
-    sleep_ms(2000); // Warten bis USB bereit ist
+    sleep_ms(2000);
 
-    // Display initialisieren
     st7789_init();
+
+    // Splash Screen
+    show_splash();
 
     // Shell starten
     shell_run();
